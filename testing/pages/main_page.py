2f3656f57a92fc4572df237b755bf83df0e7b069
Roman Kulyk <pomankyluk@gmail.com>
2024-09-24 21:40:14 +0300

    added methods for checking if product cards are located correctly in ascending or descending order

--- testing/pages/main_page.py
+++ testing/pages/main_page.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+
 class MainPage:
     """
     Constructs all the necessary attributes for the MainPage object.
@@ -20,11 +21,15 @@ class MainPage:
     show_all
         This is a method to define Show All button
     help_me
-       This is a method to define Help Me button 
+       This is a method to define Help Me button
     hm_form
         This is a method to define Help Me Form
     sort_item
         This is a method to define sorting functionality
+    sort_item_sel
+        This is a method to define sorting functionality with Sorting module
+    product_card_price
+        This is a method to find and define product price
     """
     def __init__(self, webdriver) -> None:
         """
@@ -39,30 +44,30 @@ class MainPage:
         """This is a method to open a certain web page."""
         self.driver.get(url)
         self.driver.implicitly_wait(3)
-    
+
     def buy_products(self) -> None:
         """This is a method to define Buy Products button."""
         self.buy_products_button = self.driver.find_element(
             By.XPATH, BUY_PRODUCTS)
-    
+
     def show_all(self) -> None:
         """This is a method to define Buy Products button."""
         wait = WebDriverWait(self.driver, 10)
         self.show_all_button = wait.until(EC.element_to_be_clickable((
             By.XPATH, SHOW_ALL)))
-                
+
     def help_me(self) -> None:
         """This is a method to define Help Me button."""
         self.help_me_button = self.driver.find_element(
             By.XPATH, HELP_ME)
         self.help_me_button.click()
-        
+
     def hm_form(self) -> None:
         """This is a method to define Help Me Form."""
         self.help_me_form = self.driver.find_element(
             By.XPATH, HELP_ME_FORM)
 
-    def product_card(self, index ) -> None:
+    def product_card(self, index) -> None:
         """This is a method to define Product card."""
         wait = WebDriverWait(self.driver, 10)
         self.product_card_n = wait.until(EC.element_to_be_clickable((
@@ -71,20 +76,20 @@ class MainPage:
 
     def product_cards(self) -> None:
         """This is a method to find and define Product card items."""
-        # Store all elements of dropdown in a list        
+        # Store all elements of dropdown in a list
         self.product_cards_n = list(self.driver.find_elements(
             By.XPATH, PRODUCT_CARDS))
 
-    def new_arrival_card(self, index ) -> None:
+    def new_arrival_card(self, index) -> None:
         """This is a method to define Product card."""
         wait = WebDriverWait(self.driver, 10)
         self.new_arrival_card_n = wait.until(EC.element_to_be_clickable((
             By.XPATH, f"{NA_PR_CARD}[{index}]")))
         self.new_arrival_card_n.click()
-    
+
     def new_arrival_cards(self) -> None:
         """This is a method to find and define Product card items."""
-        # Store all elements of dropdown in a list        
+        # Store all elements of dropdown in a list
         self.new_arrival_cards_n = list(self.driver.find_elements(
             By.XPATH, NA_PRODUCT_CARDS))
 
@@ -94,3 +99,17 @@ class MainPage:
         self.dropdown_list.click()
         self.sort_menu_item = self.driver.find_element(By.XPATH, locator)
         self.sort_menu_item.click()
+
+    def sort_item_sel(self) -> None:
+        """This is a method to define sorting functionality with Select module."""
+        self.dropdown_list_sel = self.driver.find_element(By.XPATH, SORT)
+        self.dropdown_list_sel.click()
+        assert self.dropdown_list_sel.is_displayed()
+        self.dropdown_list_sel.click()
+
+    def product_card_price(self) -> int:
+        """This is a method to find and define product price."""
+        self.product_card_price_value = self.driver.find_element(
+            By.XPATH, PR_CARD_PRICE)
+        text = self.product_card_price_value.text
+        return int(text[0:-2])
--- testing/tests/test_sorting.py
+++ testing/tests/test_sorting.py
@@ -2,25 +2,24 @@ from testing.pages.main_page import *
 from testing.lib.constants import *
 import time
 import random
-from selenium.webdriver.support.ui import Select
 
 
 def test_main_page(chrome_browser: object):
     driver = chrome_browser
     main_page = MainPage(driver)
-    
+
     # 1.Open MAIN_URL page
     main_page.open_page(MAIN_URL)
-    time.sleep(3)
-    
+    time.sleep(0.5)
+
     # 2.Find and click SHOW_ALL button
     main_page.show_all()
     assert main_page.show_all_button.is_enabled()
     assert main_page.show_all_button.is_displayed()
     main_page.show_all_button.click()
-    time.sleep(3)
+    time.sleep(0.5)
 
-    # 3.Find and click SORTING dropdown list    
+    # 3.Find and click SORTING dropdown list
     # list of available options in dropdown
     sort_options = [SORT1, SORT2, SORT3]
     for options in sort_options:
@@ -30,6 +29,56 @@ def test_main_page(chrome_browser: object):
         # verify if certain options is enabled and displayed
         assert main_page.sort_menu_item.is_displayed()
         assert main_page.sort_menu_item.is_enabled()
+        time.sleep(0.5)
+
+    # 4.Find and click ascending option in Sorting dropdown
+    main_page.sort_item(SORT1)
+    time.sleep(0.5)
+
+    # 5.Verify that product cards are in ascending order
+    main_page.product_cards()
+    step = random.randint(1, 5)
+
+    product_card_prices = []
+    for e in range(1, len(main_page.product_cards_n), step):
+        time.sleep(0.5)
+        main_page.product_card(e)
         time.sleep(3)
-    
- 
+
+        product_card_prices.append(main_page.product_card_price())
+        assert main_page.product_card_price() >= 0
+        time.sleep(0.5)
+        driver.back()
+    print(product_card_prices)
+
+    step = random.randint(1, 5)
+    print(len(product_card_prices))
+    for i in range(0, (len(product_card_prices) - 1), step):
+        assert product_card_prices[i] <= product_card_prices[i + 1]
+
+    # 6.Find and click descending option in Sorting dropdown
+    main_page.sort_item(SORT2)
+    time.sleep(0.5)
+
+    # 7.Verify that product cards are in descending order
+    main_page.product_cards()
+    step = random.randint(1, 5)
+
+    product_card_prices = []
+    for e in range(1, len(main_page.product_cards_n), step):
+        time.sleep(0.5)
+        main_page.product_card(e)
+        time.sleep(3)
+
+        product_card_prices.append(main_page.product_card_price())
+        assert main_page.product_card_price() >= 0
+        time.sleep(0.5)
+        driver.back()
+    print(product_card_prices)
+
+    step = random.randint(1, 5)
+    print(len(product_card_prices))
+    for i in range(0, (len(product_card_prices) - 1), step):
+        assert product_card_prices[i] >= product_card_prices[i + 1]
+
+    assert False == True  # Is there any way to do it more smarter
