ad86aaa6017f076e5477214b2e744a271054f6d4
Roman Kulyk <pomankyluk@gmail.com>
2024-12-25 18:18:12 +0200

    removed time import and appropriate calls, updated imports

--- testing/tests/test_catalog.py
+++ testing/tests/test_catalog.py
@@ -1,10 +1,19 @@
 import pytest
 from testing.pages.main_page import MainPage
-from testing.pages.hm_form import *
-from testing.pages.main_menu import *
-from testing.pages.catalog_dropdown import *
-from testing.lib.constants import *
-import time
+from testing.pages.hm_form import HelpMeForm
+from testing.pages.main_menu import MainMenu
+from testing.pages.catalog_dropdown import CatalogDropdown
+from testing.lib.constants import MAIN_URL,\
+                                  CATALOG_DROPDOWN_ITEM_1,\
+                                  CATALOG_DROPDOWN_ITEM_2,\
+                                  CATALOG_DROPDOWN_ITEM_3,\
+                                  CATALOG_DROPDOWN_ITEM_4,\
+                                  CATALOG_DROPDOWN_ITEM_5,\
+                                  CATALOG_DROPDOWN_ITEM_6,\
+                                  CATALOG_DROPDOWN_ITEM_7,\
+                                  CATALOG_DROPDOWN_ITEM_8,\
+                                  CATALOG_DROPDOWN_ITEM_9 
+
 
 
 def test_main_menu(browser: object):
@@ -13,14 +22,12 @@ def test_main_menu(browser: object):
 
     # 1.Open MAIN_URL page
     main_page.open_page(MAIN_URL)
-    time.sleep(0.5)
 
     main_menu = MainMenu(driver)
     # 2.Hover mouse over Catalog button
     main_menu.catalog()
     assert main_menu.catalog_button.is_enabled()
     assert main_menu.catalog_button.is_displayed()
-    time.sleep(0.5)
 
     catalog_dropdown = CatalogDropdown(driver)
     # 3.Ensure that Catalog Dropdown container is displayed
@@ -43,7 +50,6 @@ def test_main_menu(browser: object):
         catalog_dropdown.catalog_dropdown_items(item)
         assert catalog_dropdown.catalog_dropdown_item_link.is_enabled()
         assert catalog_dropdown.catalog_dropdown_item_link.is_displayed()
-        time.sleep(0.5)
 
     # 4.2.Ensure that Catalog Dropdown Item is displayed and clickable (Dynamic)
     catalog_dropdown.catalog_dropdown_ul()
@@ -52,7 +58,7 @@ def test_main_menu(browser: object):
             e.click()
             assert e.is_enabled()
             assert e.is_displayed()
-            time.sleep(0.5)
+
 
 
 
