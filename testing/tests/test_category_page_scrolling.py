e02b802cb8233bb1e314b4a7ae055e5422d3cee0
Roman Kulyk <pomankyluk@gmail.com>
2024-12-25 18:21:33 +0200

    fixed issues with imports

--- testing/tests/test_category_page_scrolling.py
+++ testing/tests/test_category_page_scrolling.py
@@ -1,7 +1,6 @@
 from testing.pages.main_page import MainPage
 from testing.pages.category_page import CategoryPage
 from testing.lib.constants import MAIN_URL
-import time
 
 
 def test_main_page(browser: object):
@@ -10,27 +9,22 @@ def test_main_page(browser: object):
     
     # 1.Open MAIN_URL page
     main_page.open_page(MAIN_URL)
-    time.sleep(1)
 
     # 2.Find and click GO_TO_CATEGORY_2 button
     main_page.go_to_category_2()
-    assert main_page.go_to_category_2.is_enabled()
-    assert main_page.go_to_category_2.is_displayed()
-    main_page.go_to_category_2.click()
-    time.sleep(1)
+    assert main_page.go_to_category_2_button.is_enabled()
+    assert main_page.go_to_category_2_button.is_displayed()
+    main_page.go_to_category_2_button.click()
     
     for _ in range(4):
         # 3.Scroll page until the SHOW_MORE_BUTTON will be visible
         # using ActionChain
         category_page = CategoryPage(driver)
         category_page.show_more()
-        print(1)
         assert category_page.show_more_button.is_displayed()
         assert category_page.show_more_button.is_enabled()
 
         # 3.1.Click SHOW_MORE_BUTTON 
         category_page.show_more()
-        time.sleep(3)
         assert category_page.show_more_button.is_enabled()
         assert category_page.show_more_button.is_displayed()
-        time.sleep(3)
