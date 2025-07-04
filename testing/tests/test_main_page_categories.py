050559caa044569af1458b99b86ea796de3d4756
Roman Kulyk <pomankyluk@gmail.com>
2025-07-04 20:11:53 +0300

    test cases updated to follow PEP 8 style guide

--- testing/tests/test_main_page_categories.py
+++ testing/tests/test_main_page_categories.py
@@ -9,14 +9,15 @@ import logging
 logger = logging.getLogger('leyven_tests_logger')
 
 page_titles = ["Собаки | Лейвен",
-              "Коти | Лейвен",
-              "Сухий корм | Лейвен",
-              "Сімпаріка | Інтернет-зоомагазин Лейвен",
-              "Бренди | Лейвен - Інтернет-зоомагазин",
-              "Лейвен Блог",
-              "Будиночки, лежанки, м'які місця | Лейвен",
-              "Акції | Інтернет-зоомагазин Лейвен",
-              "Новинки | Інтернет-зоомагазин Лейвен"]
+               "Коти | Лейвен",
+               "Сухий корм | Лейвен",
+               "Сімпаріка | Інтернет-зоомагазин Лейвен",
+               "Бренди | Лейвен - Інтернет-зоомагазин",
+               "Лейвен Блог",
+               "Будиночки, лежанки, м'які місця | Лейвен",
+               "Акції | Інтернет-зоомагазин Лейвен",
+               "Новинки | Інтернет-зоомагазин Лейвен"]
+
 
 @pytest.mark.with_logging
 def test_main_page_categories(browser: object):
@@ -25,31 +26,34 @@ def test_main_page_categories(browser: object):
     driver = browser
     main_page = MainPage(driver)
     main_menu = MainMenu(driver)
-    
+
     # PRECONDITIONS: The main page is opened
     main_page.open_page(MAIN_URL)
-    logger.info(f"PRECONDITIONS: The main page: {MAIN_URL} is opened.\n{'=' * 200}")
+    logger.info(f"PRECONDITIONS: The main page: "
+                f"{MAIN_URL} is opened.\n{'=' * 200}")
     main_page.product_categories()
-    
+
     for i in range(1, len(main_page.product_categories_list) + 1):
-        # 1. Find and click each Product's category link 
+        # 1. Find and click each Product's category link
         main_page.product_category(i)
-        assert main_page.product_category_n.is_enabled(),\
-        f"Product category {page_titles[i - 1]} is not enabled."
-        assert main_page.product_category_n.is_displayed(),\
-        f"Product category {page_titles[i - 1]} is not displayed."
-        logger.info(
-            f"{i}.1. Click the '{page_titles[i - 1]}' product\'s category link.")    
+        assert main_page.product_category_n.is_enabled(), \
+            f"Product category {page_titles[i - 1]} is not enabled."
+        assert main_page.product_category_n.is_displayed(), \
+            f"Product category {page_titles[i - 1]} is not displayed."
+        logger.info(f"{i}.1. Click the '{page_titles[i - 1]}' product\'s"
+            f" category link.")
         time.sleep(1)
 
         # 2. Ensure that the the page with the correct title is opened
         current_page_title = driver.title
-        logger.info(f"{i}.2. Ensure that page with the title '{current_page_title}' is opened.")
+        logger.info(f"{i}.2. Ensure that page with the title"
+                    f" '{current_page_title}' is opened.")
         expected_title = page_titles[i-1]
-        
+
         # 3. Check that the page title is correct
         assert current_page_title == expected_title, \
-            f"Expected title is '{expected_title}', but got '{current_page_title}'"
-        logger.info(f"{i}.3. Expected title is '{expected_title}', " \
-                     f"and got '{current_page_title}'\n{'=' * 200}")
+            f"Expected title is '{expected_title}', but got "\
+            f"'{current_page_title}'"
+        logger.info(f"{i}.3. Expected title is '{expected_title}', "
+                    f"and got '{current_page_title}'\n{'=' * 200}")
         main_menu.main_logo()
--- testing/tests/test_main_products_categories.py
+++ testing/tests/test_main_products_categories.py
@@ -13,6 +13,7 @@ page_titles = ["Годування домашніх тварин і птахів
                "Товари для комфорту домашніх тварин | Лейвен",
                "Товари для прогулянок і подорожей з тваринами | Лейвен"]
 
+
 @pytest.mark.with_logging
 def test_main_products_categories(browser: object):
     """Test to ensure that the main product categories on the main page
@@ -23,37 +24,37 @@ def test_main_products_categories(browser: object):
 
     # PRECONDITIONS: The main page is opened
     main_page.open_page(MAIN_URL)
-    logger.info(f"PRECONDITIONS: The main page: {MAIN_URL}" \
+    logger.info(f"PRECONDITIONS: The main page: {MAIN_URL}"
                 f" is opened.\n{'=' * 200}")
     main_page.find_main_products_categories()
-    
+
     for i in range(1, len(main_page.main_products_categories_list) + 1):
         # 1. Find and click the main product category link
         main_page.find_main_products_categories()
-        main_page.find_main_products_category(i)  
-        assert main_page.main_product_category_n.is_enabled(), \
+        main_page.find_main_products_category(i)
+        assert main_page.main_product_category_n.is_enabled(),\
             f"Main page main product category link {i} is not enabled."
-        assert main_page.main_product_category_n.is_displayed(), \
+        assert main_page.main_product_category_n.is_displayed(),\
             f"Main page main product category link {i} is not displayed."
         time.sleep(1)
         link_text = main_page.main_products_categories_list[i - 1].text
         main_page.click_main_product_category(i)
-        logger.info(f"{i}.1. Click the '{link_text}' Main page main product " \
+        logger.info(f"{i}.1. Click the '{link_text}' Main page main product "
                     f"category link.")
         time.sleep(1)
 
         # 2. Ensure that the page with the correct title is opened
         current_page_title = driver.title
-        logger.info(f"{i}.2. Ensure that page with the title" \
+        logger.info(f"{i}.2. Ensure that page with the title"
                     f"'{current_page_title}' is opened.")
         expected_title = page_titles[i-1]
 
         # 3. Check that the page title is correct
-        assert current_page_title == expected_title, \
+        assert current_page_title == expected_title,\
         f"Expected title is '{expected_title}', but got '{current_page_title}'"
-        logger.info(f"{i}.3. Expected title is '{expected_title}', " \
+        logger.info(f"{i}.3. Expected title is '{expected_title}', "
                     f" and got '{current_page_title}'\n{'=' * 200}")
         main_menu.main_logo()
 
-# TO-DO_1 - Update login functionality in conftest.py to be able to add 
+# TO-DO_1 - Update login functionality in conftest.py to be able to add
 # timestamps to the log.
