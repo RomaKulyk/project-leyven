1d31eda7dcbfb1c8c8e8be7daca73a9b69a0355d
Roman Kulyk <pomankyluk@gmail.com>
2025-07-05 19:40:38 +0300

    added logging to test_catalog.py file

--- testing/tests/test_catalog.py
+++ testing/tests/test_catalog.py
@@ -3,63 +3,87 @@ from testing.pages.main_page import MainPage
 from testing.pages.hm_form import HelpMeForm
 from testing.pages.main_menu import MainMenu
 from testing.pages.catalog_dropdown import CatalogDropdown
-from testing.lib.constants import MAIN_URL,\
-                                  CATALOG_DROPDOWN_ITEM_1,\
-                                  CATALOG_DROPDOWN_ITEM_2,\
-                                  CATALOG_DROPDOWN_ITEM_3,\
-                                  CATALOG_DROPDOWN_ITEM_4,\
-                                  CATALOG_DROPDOWN_ITEM_5,\
-                                  CATALOG_DROPDOWN_ITEM_6,\
-                                  CATALOG_DROPDOWN_ITEM_7,\
-                                  CATALOG_DROPDOWN_ITEM_8,\
-                                  CATALOG_DROPDOWN_ITEM_9 
+from testing.lib.constants import MAIN_URL
+import logging
+import time
 
+logger = logging.getLogger('leyven_tests_logger')
 
+page_titles = ["Акваріумістика | Лейвен",
+               "Ветеринарні засоби та препарати | Лейвен",
+               "Годування домашніх тварин і птахів | Лейвен",
+               "Родентициди | Лейвен",
+               "Розпродаж | Лейвен",
+               "Різне | Лейвен",
+               "Товари для догляду за домашніми тваринами | Лейвен",
+               "Товари для комфорту домашніх тварин | Лейвен",
+               "Товари для прогулянок і подорожей з тваринами | Лейвен"]
+MAIN_PAGE_TITLE = "Інтернет-зоомагазин Лейвен ✅"
 
+@pytest.mark.with_logging
 def test_main_menu(browser: object):
+    """Test to ensure that catalog links are displayed and clickable."""
     driver = browser
     main_page = MainPage(driver)
-
-    # 1.Open MAIN_URL page
-    main_page.open_page(MAIN_URL)
-
     main_menu = MainMenu(driver)
-    # 2.Hover mouse over Catalog button
+
+    # PRECONDITIONS: The main page is opened
+    main_page.open_page(MAIN_URL)
+    logger.info(f"PRECONDITIONS: The main page: {MAIN_URL} is opened.")
+    logger.info(f"[PASSED]\n{'=' * 200}")
+
+    # 1. Ensure that the 'Каталог' button is displayed and enabled.
+    logger.info(f"1.1. Ensure that the 'Каталог' button is displayed "
+                f"and enabled.")
     main_menu.catalog()
-    assert main_menu.catalog_button.is_enabled()
-    assert main_menu.catalog_button.is_displayed()
+    assert main_menu.catalog_button.is_enabled(),\
+            f"Main page 'Каталог' button is not enabled."
+    assert main_menu.catalog_button.is_displayed(),\
+            f"Main page 'Каталог' button is not displayed."
+    logger.info(f"1.2. The 'Каталог' button is displayed "
+                f"and enabled.")
+    logger.info(f"[PASSED]\n{'=' * 200}")
 
     catalog_dropdown = CatalogDropdown(driver)
-    # 3.Ensure that Catalog Dropdown container is displayed
-    catalog_dropdown.catalog_dropdown()
-    assert catalog_dropdown.catalog_dropdown_container.is_enabled()
-    assert catalog_dropdown.catalog_dropdown_container.is_displayed()
-
-    # 4.1.Ensure that Catalog Dropdown Item is displayed and clickable (Static)
-    catalog_dropdown_items = [CATALOG_DROPDOWN_ITEM_1,
-                              CATALOG_DROPDOWN_ITEM_2,
-                              CATALOG_DROPDOWN_ITEM_3,
-                              CATALOG_DROPDOWN_ITEM_4,
-                              CATALOG_DROPDOWN_ITEM_5,
-                              CATALOG_DROPDOWN_ITEM_6,
-                              CATALOG_DROPDOWN_ITEM_7,
-                              CATALOG_DROPDOWN_ITEM_8,
-                              CATALOG_DROPDOWN_ITEM_9]
+    # 2.Ensure that Catalog Dropdown container is displayed
+    logger.info(f"2.1. Ensure that the 'Каталог' dropdown container"
+                f" is displayed and enabled.")
     
-    for item in catalog_dropdown_items:
-        catalog_dropdown.catalog_dropdown_items(item)
-        assert catalog_dropdown.catalog_dropdown_item_link.is_enabled()
-        assert catalog_dropdown.catalog_dropdown_item_link.is_displayed()
+    catalog_dropdown.catalog_dropdown()
+    assert catalog_dropdown.catalog_dropdown_container.is_enabled(),\
+            f"Main page 'Каталог' dropdown container is not enabled."
+    assert catalog_dropdown.catalog_dropdown_container.is_displayed(),\
+            f"Main page 'Каталог' dropdown container is not displayed."
+    
+    logger.info(f"2.2. The 'Каталог' dropdown container is displayed "
+                f"and enabled.")
+    logger.info(f"[PASSED]\n{'=' * 200}")
 
-    # 4.2.Ensure that Catalog Dropdown Item is displayed and clickable (Dynamic)
     catalog_dropdown.catalog_dropdown_ul()
-    for e in catalog_dropdown.catalog_dropdown_elements:
-            # Iterate the list using a for loop and click each options
-            e.click()
-            assert e.is_enabled()
-            assert e.is_displayed()
+    for i in range(len(catalog_dropdown.catalog_dropdown_elements)):
+            # 3.1-2. Find and click Catalog Dropdown link
+            link_text = catalog_dropdown.catalog_dropdown_elements[i].text
+            logger.info(f"3.{i+1}.1. Click the '{link_text}' Main page catalog "
+                    f"dropdown link.")
+            
+            catalog_dropdown.catalog_dropdown_elements[i].click()
+            assert catalog_dropdown.catalog_dropdown_elements[i].is_enabled(),\
+            f"Main page catalog dropdown link {i} is not enabled."
+            assert catalog_dropdown.catalog_dropdown_elements[i].is_displayed(),\
+            f"Main page catalog dropdown link {i} is not displayed."
 
+            logger.info(f"3.{i+1}.2. The '{link_text}' Main page catalog "
+                    f"dropdown link is clicked.")
 
+            # 3.3-4. Ensure that the page with the correct title is opened
+            current_page_title = driver.title
 
+            logger.info(f"3.{i+1}.3. Ensure that page with the title "
+                        f"'{current_page_title}' is opened.")
+            expected_title = page_titles[i]
+            assert current_page_title == expected_title,\
+            f"Expected title is '{expected_title}', but got '{current_page_title}'"
 
-  
+            logger.info(f"3.{i+1}.4. Expected title is '{expected_title}', "
+                        f" and got '{current_page_title}'")
+            logger.info(f"[PASSED]\n{'=' * 200}")
